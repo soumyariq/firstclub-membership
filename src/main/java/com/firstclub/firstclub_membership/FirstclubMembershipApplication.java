73aef9565cc78db4178d1bd1d3237a87379cb88b
smranjanmishra <soumyarmishra17@gmail.com>
2026-05-28 11:44:53 +0530

    setting the skeleton of the project

---
+++ src/main/java/com/firstclub/firstclub_membership/FirstclubMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.firstclub.firstclub_membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstclubMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstclubMembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstclub/firstclub_membership/FirstclubMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstclub.firstclub_membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstclubMembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
